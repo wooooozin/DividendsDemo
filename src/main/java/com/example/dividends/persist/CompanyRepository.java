a16a58e0d8e608dc8e144043c1236ca984167f29
wooooozin <wooooozin@gmail.com>
2023-07-30 00:36:24 +0900

    Entity 구현 및 Repository 인터페이스 구현

---
+++ src/main/java/com/example/dividends/persist/CompanyRepository.java
@@ -0,0 +1,10 @@
+package com.example.dividends.persist;
+
+import com.example.dividends.persist.entity.CompanyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
+
+}
---
+++ src/main/java/com/example/dividends/persist/DividendRepository.java
@@ -0,0 +1,10 @@
+package com.example.dividends.persist;
+
+import com.example.dividends.persist.entity.DividendEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DividendRepository extends JpaRepository<DividendEntity, Long> {
+
+}
