44a379dc705a1150b0564194ac7a749a4b6752fc
wooooozin <wooooozin@gmail.com>
2023-07-29 21:30:15 +0900

    Initial commit

---
+++ src/test/java/com/example/dividends/DividendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dividends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DividendsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
