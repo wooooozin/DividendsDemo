5bb28f2112f928e033832033a138e87e2d866c2a
wooooozin <wooooozin@gmail.com>
2023-07-31 01:24:21 +0900

    redis 의존성 추가

--- src/main/java/com/example/dividends/scheduler/ScraperScheduler.java
+++ src/main/java/com/example/dividends/scheduler/ScraperScheduler.java
@@ -12,7 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 import java.util.List;
 @Slf4j
 @Component
