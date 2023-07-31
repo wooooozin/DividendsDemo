0d7f67fb0d5914b293b536921f900ee1c1510e60
wooooozin <wooooozin@gmail.com>
2023-07-31 23:00:03 +0900

    jwt 설정 및 컨트롤러 구현

--- src/main/java/com/example/dividends/security/TokenProvider.java
+++ src/main/java/com/example/dividends/security/TokenProvider.java
@@ -1,4 +1,62 @@
 package com.example.dividends.security;
 
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.jsoup.helper.StringUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class TokenProvider {
+
+    private static final String KEY_ROLES = "roles";
+    private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60; // 1H
+
+    @Value("{spring.jwt.secret}")
+    private String secretKey;
+
+    public String generateToken(String username, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(KEY_ROLES, roles);
+
+        var now = new Date();
+        var expireDate = new Date(now.getTime() + TOKEN_EXPIRE_TIME);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.ES512, this.secretKey) // 사용할 알고리즘, 비밀키
+                .compact();
+    }
+
+    public String getUsername(String token) {
+        return this.paresClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        if (!StringUtils.hasText(token)) {
+            return false;
+        }
+        var claims = this.paresClaims(token);
+        return claims.getExpiration().before(new Date());
+    }
+
+    private Claims paresClaims(String token) {
+        try {
+            return Jwts.parser().setSigningKey(this.secretKey).parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
 }
--- src/main/java/com/example/dividends/web/AuthController.java
+++ src/main/java/com/example/dividends/web/AuthController.java
@@ -1,4 +1,42 @@
 package com.example.dividends.web;
 
+import com.example.dividends.model.contstants.Auth;
+import com.example.dividends.security.TokenProvider;
+import com.example.dividends.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final MemberService memberService;
+
+    private final TokenProvider tokenProvider;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(
+            @RequestBody Auth.SignUp request
+    ) {
+        var result = this.memberService.register(request);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> signin(
+            @RequestBody Auth.SignIn request
+    ) {
+        // 패스워드 인증
+        var member = this.memberService.authenticate(request);
+        var token = this.tokenProvider.generateToken(member.getUsername(), member.getRoles());
+        return ResponseEntity.ok(token);
+    }
+
 }
